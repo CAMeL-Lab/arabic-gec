e15f0699bc5a9c2cdb1b5f3d4438c3e5b3cc30da
balhafni <ba63@nyu.edu>
2023-10-24 14:42:08 +0400

    m2 alignment creation script

---
+++ alignment/evaluate/create_m2_files.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Set number of tasks to run
+#SBATCH -q nlp
+# SBATCH --ntasks=1
+# Set number of cores per task (default is 1)
+#SBATCH --cpus-per-task=1
+# Walltime format hh:mm:ss
+#SBATCH --time=47:59:00
+# Output and error files
+#SBATCH -o job.%J.out
+#SBATCH -e job.%J.err
+
+
+# eval "$(conda shell.bash hook)"
+# conda activate python2
+
+
+qalb14_dir=/home/ba63/gec-release/data/gec/QALB-0.9.1-Dec03-2021-SharedTasks/data/2014/dev
+qalb15_dir=/home/ba63/gec-release/data/gec/QALB-0.9.1-Dec03-2021-SharedTasks/data/2015/dev
+
+python /home/ba63/gec-release/bart-t5-new/utils/m2scorer/edit_creator.py \
+        $qalb14_dir/QALB-2014-L1-Dev.sent.no_ids \
+        $qalb14_dir/QALB-2014-L1-Dev.cor.no_ids \
+        > qalb14_dev.m2
+
+python  /home/ba63/gec-release/bart-t5-new/utils/m2scorer/edit_creator.py \
+        $qalb15_dir/QALB-2015-L2-Dev.sent.no_ids \
+        $qalb15_dir/QALB-2015-L2-Dev.cor.no_ids \
+        > qalb15_dev.m2
